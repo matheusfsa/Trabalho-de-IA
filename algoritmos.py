f5bea7b9bc48efe169c99da3ddc4b648453016af
Matheus <tetofsa15@gmail.com>
2018-04-30 14:10:07 -0300

    Agora o algoritmo aceita palavras diferentes

--- algoritmos.py
+++ algoritmos.py
@@ -7,187 +7,169 @@ import numpy as np
 import time
 
 
-def makeTSP(nCities):
-    # positions corresponde às coordenadas de cada uma das n cidades
-    # positions é uma matriz n X 2, onde n é o número de cidades
-    # np.random.rand(n,m) retorna uma matriz n x m de valores aleatórios entre 0.0 e 1.0
-    positions = 2 * np.random.rand(nCities, 2) - 1;
-    # distances corresponde às distâncias entre cada cidade
-    # distances é uma matriz n x n
-    # a matriz é inicializada
-    distances = np.zeros((nCities, nCities))
-
-    # os valores das distâncias são calculados
-    for i in range(nCities):
-        for j in range(i + 1, nCities):
-            distances[i, j] = np.sqrt(
-                (positions[i, 0] - positions[j, 0]) ** 2 + (positions[i, 1] - positions[j, 1]) ** 2);
-            distances[j, i] = distances[i, j];
-
-    return distances
+def ajustar(solucao,tabela):
+    for i in range(len(solucao)):
+        if solucao[i] == 0 and tabela[i][2] == 1:
+            solucao[i] = np.random.randint(1, 9)
+    return solucao
 
 
-def permutation(order):
-    order = tuple(order)
-    if len(order) == 1:
-        yield order
-    else:
-        for i in range(len(order)):
-            rest = order[:i] + order[i + 1:]
-            move = (order[i],)
-            for smaller in permutation(rest):
-                yield move + smaller
-
-
-def greedy(distances):
-    nCities = np.shape(distances)[0]
-    distanceTravelled = 0
-
-    # Need a version of the matrix we can trash
-    dist = distances.copy()
-
-    cityOrder = np.zeros(nCities, dtype=int)
-    cityOrder[0] = np.random.randint(nCities)
-    dist[:, int(cityOrder[0])] = np.Inf
-
-    for i in range(nCities - 1):
-        cityOrder[i + 1] = np.argmin(dist[cityOrder[i], :])
-        distanceTravelled += dist[cityOrder[i], cityOrder[i + 1]]
-        # Now exclude the chance of travelling to that city again
-        dist[:, cityOrder[i + 1]] = np.Inf
-
-    # Now return to the original city
-    distanceTravelled += distances[cityOrder[nCities - 1], 0]
-
-    return cityOrder, distanceTravelled
-
-
-def hillClimbing(distances):
+def hillClimbing(tabela):
     # np.shape(v) retorna um vetor que corresponde à dimensão da matriz v
 
-    nCities = np.shape(distances)[0]
+    nLetras = len(tabela)
 
     # np.arange(n) retorna um vetor com de tamanho n com a sequência de 0 a n-1
-    cityOrder = np.arange(nCities)
-
-    # np.shuffle(v) bagunça os elementos de v
-    np.random.shuffle(cityOrder)
-
-    distanceTravelled = 0
-    for i in range(nCities - 1):
-        distanceTravelled += distances[cityOrder[i], cityOrder[i + 1]]
-    distanceTravelled += distances[cityOrder[nCities - 1], 0]
-
-    for i in range(1000):
+    valores = np.random.randint(10, size = nLetras)
+    soma = qualidade(valores, tabela)
+    n_interacoes = 0
+    for i in range(3000):
         # Choose cities to swap
-        city1 = np.random.randint(nCities)
-        city2 = np.random.randint(nCities)
+        pos = np.random.randint(nLetras)
+        pos2 = np.random.randint(nLetras)
+        possibleValues = valores.copy()
+        possibleValues[pos] = np.random.randint(10)
+        possibleValues[pos2] = np.random.randint(10)
+        possibleValues = ajustar(possibleValues,tabela)
+        novaSoma = qualidade(possibleValues,tabela)
 
-        if city1 != city2:
-            # Reorder the set of cities
-            possibleCityOrder = cityOrder.copy()
-            possibleCityOrder = np.where(possibleCityOrder == city1, -1, possibleCityOrder)
-            possibleCityOrder = np.where(possibleCityOrder == city2, city1, possibleCityOrder)
-            possibleCityOrder = np.where(possibleCityOrder == -1, city2, possibleCityOrder)
-
-            # Work out the new distances
-            # This can be done more efficiently
-            newDistanceTravelled = 0
-            for j in range(nCities - 1):
-                newDistanceTravelled += distances[possibleCityOrder[j], possibleCityOrder[j + 1]]
-            distanceTravelled += distances[cityOrder[nCities - 1], 0]
-
-            if newDistanceTravelled < distanceTravelled:
-                distanceTravelled = newDistanceTravelled
-                cityOrder = possibleCityOrder
-
-    return cityOrder, distanceTravelled
+        if novaSoma < soma:
+            soma = novaSoma
+            valores = possibleValues
+        n_interacoes += 1
+    return valores, soma, n_interacoes
 
 
-def simulatedAnnealing(distances):
-    nCities = np.shape(distances)[0]
+def simulatedAnnealing(nLetras):
 
-    cityOrder = np.arange(nCities)
-    np.random.shuffle(cityOrder)
+    valores = np.random.randint(10, size=nLetras)
+    soma = quality(valores)
 
-    distanceTravelled = 0
-    for i in range(nCities - 1):
-        distanceTravelled += distances[cityOrder[i], cityOrder[i + 1]]
-    distanceTravelled += distances[cityOrder[nCities - 1], 0]
-
-    T = 500
+    T = 1000
     c = 0.8
-    nTests = 1000
+    nTests = 5000
 
     # while T>1:
+    n_interacoes = 0
     for i in range(nTests):
         # Choose cities to swap
-        city1 = np.random.randint(nCities)
-        city2 = np.random.randint(nCities)
+        pos = np.random.randint(nLetras)
+        pos2 = np.random.randint(nLetras)
+        possibleValues = valores.copy()
+        possibleValues[pos] = np.random.randint(10)
+        possibleValues[pos2] = np.random.randint(10)
+        possibleValues = ajustar(possibleValues)
+        novaSoma = quality(possibleValues)
 
-        if city1 != city2:
-            # Reorder the set of cities
-            possibleCityOrder = cityOrder.copy()
-            possibleCityOrder = np.where(possibleCityOrder == city1, -1, possibleCityOrder)
-            possibleCityOrder = np.where(possibleCityOrder == city2, city1, possibleCityOrder)
-            possibleCityOrder = np.where(possibleCityOrder == -1, city2, possibleCityOrder)
-
-            # Work out the new distances
-            # This can be done more efficiently
-            newDistanceTravelled = 0
-            for j in range(nCities - 1):
-                newDistanceTravelled += distances[possibleCityOrder[j], possibleCityOrder[j + 1]]
-            distanceTravelled += distances[cityOrder[nCities - 1], 0]
-
-            if (newDistanceTravelled < distanceTravelled) or (
-                (distanceTravelled - newDistanceTravelled) > T * np.log(np.random.rand())):
-                distanceTravelled = newDistanceTravelled
-                cityOrder = possibleCityOrder
+        if (novaSoma < soma) or (
+            (soma - novaSoma) > T * np.log(np.random.rand())):
+            soma = novaSoma
+            valores = possibleValues
 
         # Annealing schedule
         T = c * T
+        n_interacoes += 1
+    return valores, soma, n_interacoes
 
-    return cityOrder, distanceTravelled
+def imprimeResultado(vetor,tabela):
+    print("Resultado:")
+    for i in range(len(vetor)):
+        print("{} = {} ".format(chr(tabela[i][0]+65), vetor[i]))
+    #print("S = ", vetor[0], " E = ", vetor[1], " N = ", vetor[2], " D = ", vetor[3], " M = ", vetor[4], " O = ",
+     #     vetor[5], " R = ", vetor[6], " Y = ", vetor[7])
+    #print("  {}{}{}{}".format(vetor[0], vetor[1], vetor[2], vetor[3]))
+    #print("+ {}{}{}{}".format(vetor[4], vetor[5], vetor[6], vetor[1]))
+    #print("----------")
+    #print(" {}{}{}{}{}".format(vetor[4], vetor[5], vetor[2], vetor[1], vetor[7]))
 
-
-def parameters(nCities):
-    #	import time
-    #	nCities = ncities
-    distances = makeTSP(nCities)
-    return distances
-
-
-def runExaustive(distances):
-    print("Busca exaustiva")
-    start = time.time()
-    result = exhaustive(distances)
-    finish = time.time()
-    print("Ordem:", result[0], " Distancia:", result[1])
-    print("Tempo:", finish - start)
-
-
-def runGreedy(distances):
-    print("\nGreedy")
-    start = time.time()
-    result = greedy(distances)
-    finish = time.time()
-    print("Ordem:", result[0], " Distancia:", result[1])
-    print("Tempo:", finish - start)
-
-
-def runHill(distances):
+def vetores():
+    n = int(input("Quantos termos serão somados?\n"))
+    print("Digite os termos da soma\n")
+    res = [0 for i in range(n+1)]
+    for i in range(n):
+        res[i] = input("Termo " + str(i) + ": ")
+    res[n] = input("Digite o termo correspondente ao resultado:")
+    return res
+def runHill():
     print("\nHill Climbing")
+    tabela = criaTabelaRes(["SEND", "MORE", "MONEY"])
     start = time.time()
-    result = hillClimbing(distances)
+    result = hillClimbing(tabela)
     finish = time.time()
-    print("Ordem:", result[0], " Distancia:", result[1])
-    print("Tempo:", finish - start)
+    imprimeResultado(result[0],tabela)
+    print(" Número de Interações: ", result[2])
+    print(" Resultado:", result[1])
+    print("Tempo:", finish-start)
 
 
-def runSimAnnealing(distances):
+def runSimAnnealing(nLetras):
     print("\nSimulated Annealing")
     start = time.time()
-    result = simulatedAnnealing(distances)
+    result = simulatedAnnealing(nLetras)
     finish = time.time()
-    print("Ordem:", result[0], " Distancia:", result[1])
+    imprimeResultado(result[0])
+    print(" Resultado:", result[1])
+    print(" Número de Interações: ", result[2])
     print("Tempo:", finish - start)
+
+
+def mapeamento(x):
+    pos = ord(x.upper())-65
+    return pos
+
+#runHill()
+#runSimAnnealing(8)
+
+#print(mapeamento("a",[3,1,2,6,4]))
+#tabela = [0 for i in range(27)]
+
+def criaTabelaRes(vetores):
+    j=0
+    tam = len(vetores)
+    tabela = [(0,0,0) for i in range(27)]
+    for i in range(tam):
+        tam_i = len(vetores[i])
+        if i != tam-1:
+            for j in range(tam_i):
+                pos = ord(vetores[i][j].upper()) - 65;
+                valor1 = pos
+                valor2 = tabela[pos][1] + 10**(tam_i - (j+1))
+                #print("{} {} {}".format(vetores[i][j].upper(), pos, 10**(tam_i - (j+1))))
+                if j == 0:
+                    tabela[pos] = (valor1, valor2,1)
+                else:
+                    tabela[pos] = (valor1, valor2, 0)
+        else:
+            for j in range(tam_i):
+                pos = ord(vetores[i][j].upper()) - 65;
+                valor1 = pos
+                valor2 = tabela[pos][1] - 10 ** (tam_i - (j + 1))
+                #print("{} {} {}".format(vetores[i][j].upper(), pos, 10 ** (tam_i - (j + 1))))
+                if j == 0:
+                    tabela[pos] = (valor1, valor2,1)
+                else:
+                    tabela[pos] = (valor1, valor2, 0)
+    tabela_res = [x for x in tabela if x[0] != 0]
+    return tabela_res
+
+
+def soma(v,t):
+    termo = 0
+    tam = len(v)
+    for i in range(len(v)):
+        valor = mapeamento(v[i])
+        termo += valor * (10 ** (tam - (i + 1)))
+    return termo
+
+
+def qualidade(vetor, tabela):
+    res = 0
+    for v in range(len(vetor)):
+        res += vetor[v] * tabela[v][1]
+    return abs(res)
+
+#print(qualidade(["ABC", "BBA", "ACB"], [2, 2, 3]))
+tabela = criaTabelaRes(["SEND","MORE", "MONEY"])
+res = qualidade([4, 3, 1, 3, 0, 0, 9, 7],tabela)
+print(res)
+runHill()
